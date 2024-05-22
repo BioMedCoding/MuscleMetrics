e946524634a5e7a39f278a7c48076e2153a726f7
grovelling <matteo.o@outlook.it>
2024-05-22 11:25:25 +0200

    1. Creazione script ICA

--- Functions/BSS/TestICA_Students.m
+++ Functions/BSS/TestICA_Students.m
@@ -8,6 +8,8 @@ close all;
 clear
 clc
 
+% Obiettivo è ricostruire le 3 sorgenti dopo aver rimosso il blink
+
 %% mixing matrix
 M=[-1 2 3; 2 -2 1; 2 -1 -2];
 
@@ -25,7 +27,7 @@ x3 = randn(1,N);
 sigma = 1;
 x2 = exp(-t.^2/2/sigma^2);
 % Vettore delle sorgenti (mettere in un vettore le 3 sorgenti simulate)
-x = vertcat(x1, x2, x3);
+x = vertcat(x1, x2(1:length(x1)), x3);
 
 % normalizzare le sorgenti usando la funzione zscore
 for i=1:3
@@ -43,18 +45,24 @@ ic = W * y;
 
 % controllare che le IC calcolate nella riga precedente siano uguali alle
 % icasig
+% Segnali sono esattamente sovrapposti
+figure
 for i=1:3
     subplot(3,1,i);
     plot(icasig(i,:));
-    hold on;plot(ic(i,:));
+    hold on;
+    plot(ic(i,:));
     title(['IC' num2str(i)]);
 end
 
+
+
 % Ricostruzione delle miscele usando le IC e la matrice di mixing
-yest= A * icasig;
+yest = A * icasig;
 
 % test che le miscele ricostruite siano davvero uguali alle miscele
 % iniziali
+% Sono effettivamente sovrapposte
 figure;
 for i=1:3
     subplot(3,1,i);
@@ -71,6 +79,7 @@ end
 
 % vettore delle sorgenti buone (sottraggo index_IC dal vettore 1:3)
 vet_good_IC=setdiff(1:3,index_IC);
+% 1:3 crea un vettore [1, 2, 3], che rappresenta gli indici di tutte le componenti indipendenti (IC) estratte dal segnale misto y
 
 % ricostruzione delle miscele usando solo le sorgenti buone
 %y_artifact_removed=...;
@@ -85,4 +94,15 @@ for i=1:3
     plot(y_artifact_removed(i,:));
     title(['mixture ' num2str(i) ' without the artifact']);
 end
+
+% figure
+% subplot(3,1,1);
+% plot(x1);
+% 
+% subplot(3,1,2);
+% plot(x2);
+% 
+% subplot(3,1,3);
+% plot(x3);
+% title(['Segnali originali']);
 
